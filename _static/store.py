67355a6fde888bfaf1290c97e4f2d6a22b08b543
Alina Marcus <alinamarcus@Mac.fritz.box>
2025-06-11 18:44:13 +0200

    Added Store class and fixed import from products.py to store.py

--- _static/store.py
+++ _static/store.py
@@ -1,57 +1,42 @@
-class Product:
+import products
 
-    def __init__(self, name, price, quantity):
-        if not name:
-            raise ValueError("Product name is required")
-        if quantity < 0:
-            raise ValueError("Quantity must be positive")
+class Store:
+    def __init__(self, products=None):
+        if products is None:
+            products = []
+        self.products = products
 
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.active = True
+    def add_product(self, product):
+        self.products.append(product)
 
-    def get_quantity(self):
-        return self.quantity
+    def remove_product(self, product):
+        self.products.remove(product)
 
-    def set_quantity(self, quantity):
-        self.quantity = quantity
-        if self.quantity == 0:
-            self.active = False
+    def get_total_quantity(self):
+        return len(self.products)
 
-    def is_active(self):
-        return self.active
+    def get_all_products(self):
+        active_products = []
+        for product in self.products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
 
-    def activate(self):
-        self.active = True
+    def order(self, shopping_list):
+        total_price = 0
+        for product, quantity in shopping_list:
+            result = product.buy(quantity)
+            if isinstance(result, str):
+                print(f"Could not buy {product.name}: {result}")
+            else:
+                total_price += result
+        return total_price
 
-    def deactivate(self):
-        self.active = False
 
-    def show(self):
-        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
 
-    def buy(self, quantity_bought):
-        if quantity_bought > self.quantity:
-            return "Not enough stock available"
-        if self.quantity == 0 or not self.active:
-            return "This product is not available"
+bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = products.Product("MacBook Air M2", price=1450, quantity=100)
 
-        self.quantity -= quantity_bought
-        self.set_quantity(self.quantity)
-        total_price = self.price * quantity_bought
-        return f"Great choice! You bought {self.name} x {quantity_bought}. Total price: {self.price * quantity_bought}"
-
-# Test
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-bose.show()
-print(mac.show())
-
-bose.set_quantity(1000)
-print(bose.show())
+best_buy = Store([bose, mac])
+price = best_buy.order([(bose, 5), (mac, 30), (bose, 10)])
+print(f"Order cost: {price} dollars.")
